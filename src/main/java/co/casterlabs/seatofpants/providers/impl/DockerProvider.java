f4ec75732717462d79b4c7140253e0053f7520df
e3ndr <33337309+e3ndr@users.noreply.github.com>
2025-03-06 07:23:13 -0600

    Docker: Always pull image.

--- src/main/java/co/casterlabs/seatofpants/providers/impl/DockerProvider.java
+++ src/main/java/co/casterlabs/seatofpants/providers/impl/DockerProvider.java
@@ -73,6 +73,7 @@ public class DockerProvider implements InstanceProvider {
             CommandBuilder command = new CommandBuilder()
                 .add("docker", "run")
                 .add("--rm")
+                .add("--pull", "always")
                 .add("--name", idToUse)
                 .add("-p", String.format("127.0.0.1:%d:%d", port, this.config.portToMap));
             for (Map.Entry<String, String> entry : this.config.env.entrySet()) {
