6d62ddc52330efc023729f94300c02311366b3f3
e3ndr <33337309+e3ndr@users.noreply.github.com>
2025-05-26 14:53:04 -0500

    Debugging, hopefully.

--- docker_launch.sh
+++ docker_launch.sh
@@ -3,9 +3,13 @@
 mkdir -p data
 cd data
 
+if [[ -n "$JMX_PORT" ]]; then
+	jmx_properties="-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=$JMX_PORT -Dcom.sun.management.jmxremote.rmi.port=$JMX_PORT -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname=localhost"
+fi
+
 java \
 	-XX:+AlwaysPreTouch -XX:+UseNUMA \
 	-XX:+ExplicitGCInvokesConcurrent -XX:+UseCompressedOops \
 	-XX:+UseShenandoahGC -XX:+UnlockExperimentalVMOptions -XX:+ShenandoahPacing \
 	-XX:MinHeapFreeRatio=15 -XX:MaxHeapFreeRatio=30 \
-	-jar ../seatofpants.jar
+	$jmx_properties -jar ../seatofpants.jar
