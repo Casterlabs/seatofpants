639e2458ecf56c4a0a9eea4bd453707ea9661cad
e3ndr <33337309+e3ndr@users.noreply.github.com>
2025-05-26 15:01:11 -0500

    Hopefully fix JMX.

--- docker_launch.sh
+++ docker_launch.sh
@@ -3,8 +3,8 @@
 mkdir -p data
 cd data
 
-if [[ -n "$JMX_PORT" ]]; then
-	jmx_properties="-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=$JMX_PORT -Dcom.sun.management.jmxremote.rmi.port=$JMX_PORT -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname=localhost"
+if [[ -n $JMX_PORT ]]; then
+	jmx_properties="-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=$JMX_PORT -Dcom.sun.management.jmxremote.rmi.port=$JMX_PORT -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname=0.0.0.0"
 fi
 
 java \
